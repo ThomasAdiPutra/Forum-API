8bc635a81468f8c604dc2b43660288304f7bc04e
Thomas <thomasadiputra@gmail.com>
2023-08-14 15:55:15 +0700

    Fix test

--- src/Domains/threads/entities/_test/AddThread.test.js
+++ src/Domains/threads/entities/_test/AddThread.test.js
@@ -9,7 +9,7 @@ describe('a AddThread entities', () => {
     };
 
     // Action and Assert
-    // expect(() => new AddThread(payload)).toThrowError('ADD_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
+    expect(() => new AddThread(payload)).toThrowError('ADD_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
   });
 
   it('should throw error when payload did not meet data type specification', () => {
